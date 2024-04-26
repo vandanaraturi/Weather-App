bb4d485962265232c8914765ae547192da1de111
vandanaraturi <118027622+vandanaraturi@users.noreply.github.com>
2024-04-27 01:05:48 +0530

    Add files via upload

---
+++ weatherApp/script.js
@@ -0,0 +1,64 @@
+  
+  window.addEventListener('load', () => {
+  const apiKey= "ee5568c8f520edc6665816570cd77f03";
+  const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=banglore";
+  // const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+  
+   const searchBox=document.querySelector(".search input");
+   const searchBtn=document.querySelector(".search button");
+   const weatherIcon= document.querySelector(".weather-icon");
+    
+  
+    async function checkWeather(city){
+    const response= await fetch(apiUrl + city + `&apiKey=${apiKey}`);
+   
+if(response.status==404){
+
+ document.querySelector(".error").style.display="block";
+ document.querySelector(".weather").style.display="none";
+ 
+}
+else{
+  
+   var data= await response.json();
+
+   // console.log(data);
+
+    document.querySelector(".city").innerHTML=data.name;
+    document.querySelector(".temp").innerHTML=Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML=data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML=data.wind.speed + " km/h";
+    
+    if(data.weather[0].main=="Clouds")
+    {
+     weatherIcon.src="images/clouds.png";
+    }
+    else if(data.weather[0].main=="Clear")
+    {
+     weatherIcon.src="images/clear.png";
+    }
+    else if(data.weather[0].main=="Rain")
+    {
+     weatherIcon.src="images/rain.png";
+    }
+    else if(data.weather[0].main=="Drizzle")
+    {
+     weatherIcon.src="images/drizzle.png";
+    }
+    else if(data.weather[0].main=="Mist")
+    {
+     weatherIcon.src="images/Mist.png";
+    }
+  
+    document.querySelector(".weather").style.display="block";
+    document.querySelector(".error").style.display="none";
+      
+    }
+  }
+
+  searchBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    checkWeather(searchBox.value);
+});
+  });
